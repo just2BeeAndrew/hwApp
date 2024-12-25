6b3967f7bb5e97bfd062aa94bb628e9573d4b65a
Andrew Dudal <andrew.dudal.1997@gmail.com>
2024-12-26 00:17:20 +0300

    fix vercel.json

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ const startApp = async () => {
     const res = await runDb(SETTINGS.MONGO_URL)
     if (!res) process.exit(1)
 
-
     app.listen(SETTINGS.PORT, () => {
         console.log('...server started in port ' + SETTINGS.PORT)
     })
