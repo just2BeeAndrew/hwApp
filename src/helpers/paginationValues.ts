76738bc57d4c67af0935cbf1cd658c5961eb4c0b
just2BeeAndrew <andrew.dudal.1997@gmail.com>
2025-01-13 17:20:06 +0300

    fix pagination and validation

--- src/helpers/paginationValues.ts
+++ src/helpers/paginationValues.ts
@@ -7,10 +7,12 @@ export type SortType = {
     sortDirection: SortDirection
     pageNumber: number
     pageSize: number
+    searchLoginTerm: string | null
+    searchEmailTerm: string | null
 }
 
 export const paginationQueries = (req: Request):SortType => {
-    let searchNameTerm:string | null = req.query.searchNameTerm ? req.query.searchNameTerm.toString() : null;
+    let searchNameTerm: string | null = req.query.searchNameTerm ? req.query.searchNameTerm.toString() : null;
     let sortBy:string = req.query.sortBy ? req.query.sortBy.toString() : "createdAt";
     let sortDirection: 'asc'|'desc' =
         req.query.sortDirection && req.query.sortDirection.toString() === 'asc'
@@ -18,5 +20,7 @@ export const paginationQueries = (req: Request):SortType => {
             : 'desc'
     let pageNumber: number = req.query.pageNumber ? +req.query.pageNumber : 1;
     let pageSize:number = req.query.pageSize ? +req.query.pageSize : 10;
-    return {pageNumber, pageSize, sortBy, sortDirection, searchNameTerm};
+    let searchLoginTerm: string | null = req.query.searchNameTerm ? req.query.searchNameTerm.toString() : null;
+    let searchEmailTerm: string | null = req.query.searchEmailTerm ? req.query.searchEmailTerm.toString() : null;
+    return {searchNameTerm, sortBy, sortDirection, pageNumber, pageSize, searchLoginTerm, searchEmailTerm};
 }
