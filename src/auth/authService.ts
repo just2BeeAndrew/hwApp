c4dce50c93d11a8ebb528471fa863bc21f4050b3
Andrew Dudal <andrew.dudal.1997@gmail.com>
2025-02-07 13:39:15 +0300

    update checkCredentials

--- src/auth/authService.ts
+++ src/auth/authService.ts
@@ -1,4 +1,4 @@
-import {LoginInputType, UserAccountDBType, UserDBType} from "../types/db.types";
+import {UserAccountDBType} from "../types/db.types";
 import {WithId} from "mongodb";
 import {usersRepository} from "../users/usersRepository";
 import {Result} from "../result/result.type";
@@ -7,7 +7,7 @@ import {bcryptService} from "../application/bcryptService";
 import {jwtService} from "../application/jwtService";
 
 export const authService = {
-    async loginUser(loginOrEmail:string, password: string): Promise<Result<{ accessToken: string } | null>> {
+    async loginUser(loginOrEmail: string, password: string): Promise<Result<{ accessToken: string } | null>> {
         const result = await this.checkCredentials(loginOrEmail, password);
 
         if (result.status !== ResultStatus.Success)
@@ -27,8 +27,9 @@ export const authService = {
         }
     },
 
-    async checkCredentials(loginOrEmail:string, password:string): Promise<Result<WithId<UserAccountDBType> | null>> {
+    async checkCredentials(loginOrEmail: string, password: string): Promise<Result<WithId<UserAccountDBType> | null>> {
         const user = await usersRepository.findByLoginOrEmail(loginOrEmail);
+
         if (!user) {
             return {
                 status: ResultStatus.NotFound,
@@ -37,7 +38,18 @@ export const authService = {
                 extensions: [{field: 'loginOrEmail', message: 'Not Found'}],
             }
         }
+
+        if (!user.emailConfirmation.isConfirm) {
+            return {
+                status: ResultStatus.BadRequest,
+                errorMessage: "Bad Request",
+                extensions: [{field: 'user', message: "user isn't confirmed"}],
+                data: null
+            }
+        }
+
         const passwordHash = await bcryptService.checkPassword(password, user.accountData.passwordHash)
+
         if (!passwordHash) {
             return {
                 status: ResultStatus.BadRequest,
